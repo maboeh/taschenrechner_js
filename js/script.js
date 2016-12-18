219e03e4c2893cfbd21b95950190c736bfcbbc3d
Matthias Böhnke <maboeh99@googlemail.com>
2016-12-18 21:37:54 +0100

    nfangsversion

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var eins_value = document.getElementById("1").value;
+var eins = document.getElementById("1");
+var p = document.getElementById("p");
+var clear = document.getElementById("c");
+var zwei_value = document.getElementById("2").value;
+var zwei = document.getElementById("2");
+var plus_value = document.getElementById("+").value;
+var plus = document.getElementById("+");
+	
+eins.onclick = function(){
+	document.form.anzeige.value = eins_value;
+};
+clear.onclick = function(){
+
+	document.getElementById("textarea").value = "";
+}
+zwei.onclick = function(){
+	document.form.anzeige.value = zwei_value;
+}
+plus.onclick = function(){
+		document.form.anzeige.value = plus_value;
+}
