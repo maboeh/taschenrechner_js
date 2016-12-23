e83ca3470253ee92aa7de6d027f38279a90ab9fc
Matthias Böhnke <maboeh99@googlemail.com>
2016-12-23 16:14:41 +0100

    23.12.2016

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-var eins_value = document.getElementById("1").value;
-var eins = document.getElementById("1");
-var p = document.getElementById("p");
 var clear = document.getElementById("c");
-var zwei_value = document.getElementById("2").value;
-var zwei = document.getElementById("2");
-var plus_value = document.getElementById("+").value;
-var plus = document.getElementById("+");
-var gleich_value = document.getElementById("=").value;
-var gleich = document.getElementById("=");
-var speicher = [];
-
-
-
 clear.onclick = function() {
 
     document.getElementById("textarea").value = "";
